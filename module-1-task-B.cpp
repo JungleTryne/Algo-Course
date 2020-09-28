afecb07f46da8792d784da4c779f63b28e10c4a2
Mishin Danila <danila.mishin.2001@gmail.com>
2020-09-28 21:14:10 +0300

    formatting fix

--- module-1-task-B.cpp
+++ module-1-task-B.cpp
@@ -21,15 +21,15 @@ void PalindromeHandler::GetAmount(bool even) {
     std::vector<int64_t> palindrome_count(text_.size());
     auto bias = static_cast<int64_t>(even);
     
-    for(int64_t i = 0; i < static_cast<int64_t>(text_.size()); ++i) {
+    for (int64_t i = 0; i < static_cast<int64_t>(text_.size()); ++i) {
         auto answer = static_cast<int64_t>(!even); //Min palindrome length is 1 (for odd-sized) or 0 (for even-sized)
-        if(i <= right) {
+        if (i <= right) {
             int64_t potential_update = palindrome_count[static_cast<size_t>(left + (right - i) + bias)];
             answer = right - i + 1 < potential_update ? right - i + 1 : potential_update;
         }
         //Trying to improve the answer in naive way
-        while(i - answer - bias > -1 && static_cast<size_t>(i + answer) < text_.size()) {
-            if(text_[static_cast<size_t>(i - answer - bias)] == text_[static_cast<size_t>(i + answer)]) {
+        while (i - answer - bias > -1 && static_cast<size_t>(i + answer) < text_.size()) {
+            if (text_[static_cast<size_t>(i - answer - bias)] == text_[static_cast<size_t>(i + answer)]) {
                 ++answer;
             } else {
                 break;
@@ -37,7 +37,7 @@ void PalindromeHandler::GetAmount(bool even) {
         }
         palindrome_count[static_cast<size_t>(i)] = answer;
         //Updating left and right bounds
-        if(right < i + answer - 1) {
+        if (right < i + answer - 1) {
             left = i - answer + (1 - bias) ;
             right = i + answer - 1;
         }
@@ -51,10 +51,10 @@ int64_t PalindromeHandler::GetAmountOfPalindromes() {
     GetAmount(true);
 
     int64_t counter = 0;
-    for(int64_t palindrome_size : odd_palindrome_) {
+    for (int64_t palindrome_size : odd_palindrome_) {
         counter += (palindrome_size - 1);
     }
-    for(int64_t palindrome_size : even_palindrome_) {
+    for (int64_t palindrome_size : even_palindrome_) {
         counter += palindrome_size;
     }
     return counter;
