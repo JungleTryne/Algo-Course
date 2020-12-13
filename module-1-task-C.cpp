18848f2de1c0ea4d1f4b26882b9a45b8da152cae
Mishin Danila <danila.mishin.2001@gmail.com>
2020-12-14 00:06:20 +0300

    refactored

---
+++ module-1-task-C.cpp
@@ -0,0 +1,209 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <unordered_map>
+#include <vector>
+#include <memory>
+
+class TemplateFinder {
+private:
+    /* Trie node */
+    struct Node {
+        bool terminal_ = false;
+        size_t word_size_ = 0;
+        char parent_char_ = 0;
+
+        std::shared_ptr<Node> parent_;
+        std::shared_ptr<Node> suffix_;
+        std::shared_ptr<Node> shrink_suffix_;
+
+        std::vector<size_t> word_bias_; //Subtemplate bias. Subtemplates can be repeated -> several biases
+        std::unordered_map<char, std::shared_ptr<Node>> transitions_;
+        std::unordered_map<char, std::shared_ptr<Node>> delta_function_;
+    };
+
+    size_t subpattern_count_ = 0;
+    size_t pattern_size_;
+
+    std::shared_ptr<Node> root_;
+    std::vector<std::shared_ptr<Node>> nodes_;
+
+    char splitter_;
+
+    void AddSubTemplate(const std::string& subtemplate, size_t word_bias);
+    void ProcessShrunk(const std::shared_ptr<Node>& current_p, size_t char_pos, std::vector<size_t>& pattern_entries);
+
+    std::shared_ptr<Node> GetSuffix(const std::shared_ptr<Node>& current_p);
+    std::shared_ptr<Node> GoDelta(const std::shared_ptr<Node>& current_p, char c);
+    std::shared_ptr<Node> GetShrunkSuffix(const std::shared_ptr<Node>& current_p);
+
+    static void UpdateEntries(const std::shared_ptr<Node>& current_p, size_t char_position,
+                              std::vector<size_t>& pattern_entries);
+
+    static auto Split(const std::string& text, char splitter)
+        -> std::pair<std::vector<std::string>, std::vector<size_t>>;
+public:
+    explicit TemplateFinder(const std::string& pattern, char splitter);
+
+    template<typename OutputIterator>
+    void FindEntries(const std::string& text, OutputIterator& out);
+};
+
+/* Adding subtemplate to trie */
+void TemplateFinder::AddSubTemplate(const std::string &subtemplate, size_t word_bias) {
+    auto p_current = root_;
+    for (char c : subtemplate) {
+        if (p_current->transitions_.find(c) == p_current->transitions_.end()) {
+            p_current->transitions_[c] = std::make_shared<Node>();
+            p_current->transitions_[c]->parent_ = p_current;
+            p_current->transitions_[c]->parent_char_ = c;
+            nodes_.push_back(p_current->transitions_[c]);
+        }
+        p_current = p_current->transitions_[c];
+    }
+    p_current->terminal_ = true;
+    p_current->word_bias_.push_back(word_bias);
+    p_current->word_size_ = subtemplate.size();
+    ++subpattern_count_;
+}
+
+TemplateFinder::TemplateFinder(const std::string& pattern, char splitter) : pattern_size_(pattern.size()),
+                                                                            splitter_(splitter) {
+    root_ = std::make_shared<Node>();
+    nodes_.push_back(root_);
+    auto [split_text, bias] = Split(pattern, splitter_);
+    for (size_t i = 0; i < split_text.size(); ++i) {
+        AddSubTemplate(split_text[i], bias[i]);
+    }
+}
+
+/* Splitting the template to subtemplates */
+auto TemplateFinder::Split(const std::string &text, char splitter)
+    -> std::pair<std::vector<std::string>, std::vector<size_t>>
+{
+    std::vector<std::string> split_text;
+    std::vector<size_t> bias; //Position of subtemplates in the template
+    std::string buffer;
+
+    size_t counter = 0;
+    for (char c : text) {
+        if (c == splitter && !buffer.empty()) {
+            bias.push_back(counter - buffer.size());
+            split_text.push_back(buffer);
+            buffer = "";
+        } else if (c != splitter) {
+            buffer += c;
+        }
+        ++counter;
+    }
+    if (!buffer.empty()) {
+        bias.push_back(counter - buffer.size());
+        split_text.push_back(buffer);
+    }
+    return std::make_pair(split_text, bias);
+}
+
+/* Getting suffix link of the node */
+auto TemplateFinder::GetSuffix(const std::shared_ptr<Node>& current_p)
+    -> std::shared_ptr<Node>
+{
+    if (!current_p->suffix_) {
+        if (current_p == root_ || current_p->parent_ == root_) {
+            current_p->suffix_ = root_;
+        } else {
+            current_p->suffix_ = GoDelta(GetSuffix(current_p->parent_), current_p->parent_char_);
+        }
+    }
+    return current_p->suffix_;
+}
+
+/* Delta function of automata */
+auto TemplateFinder::GoDelta(const std::shared_ptr<Node>& current_p, char c)
+    -> std::shared_ptr<Node>
+{
+    if (current_p->delta_function_.find(c) == current_p->delta_function_.end()) {
+        if (current_p->transitions_.find(c) != current_p->transitions_.end()) {
+            current_p->delta_function_[c] = current_p->transitions_[c];
+        } else if (current_p == root_) {
+            current_p->delta_function_[c] = root_;
+        } else {
+            current_p->delta_function_[c] = GoDelta(GetSuffix(current_p), c);
+        }
+    }
+    return current_p->delta_function_[c];
+}
+
+/* Getting shrunk suffix link of the node */
+auto TemplateFinder::GetShrunkSuffix(const std::shared_ptr<Node>& current_p)
+    -> std::shared_ptr<Node>
+{
+    if (!current_p->shrink_suffix_) {
+        std::shared_ptr<Node> suffix_link = GetSuffix(current_p);
+        if (suffix_link->terminal_) {
+            current_p->shrink_suffix_ = suffix_link;
+        } else if (suffix_link == root_) {
+            current_p->shrink_suffix_ = root_;
+        } else {
+            current_p->shrink_suffix_ = GetShrunkSuffix(suffix_link);
+        }
+    }
+    return current_p->shrink_suffix_;
+}
+
+/* Main algorithm function - finding pattern in the text  */
+template<typename OutputIterator>
+void TemplateFinder::FindEntries(const std::string &text, OutputIterator& out) {
+    std::shared_ptr<Node> current_p = root_;
+    std::vector<size_t> pattern_entries(text.size());
+    
+    for (size_t char_pos = 0; char_pos < text.size(); ++char_pos) {
+        current_p = GoDelta(current_p, text[char_pos]);
+        ProcessShrunk(current_p, char_pos, pattern_entries);
+
+        if (current_p->terminal_) {
+            UpdateEntries(current_p, char_pos, pattern_entries);
+        }
+    }
+
+    for (size_t char_pos = 0; char_pos < pattern_entries.size(); ++char_pos) {
+        if (pattern_entries[char_pos] == subpattern_count_ && char_pos + pattern_size_ < text.size() + 1) {
+            *out = char_pos;
+            ++out;
+        }
+    }
+}
+
+/* Shrunk suffix traversal */
+auto TemplateFinder::ProcessShrunk(const std::shared_ptr<Node>& current_p, size_t char_pos,
+                                   std::vector<size_t> &pattern_entries) -> void
+{
+    for (auto shrunk_p = GetShrunkSuffix(current_p); shrunk_p != root_; shrunk_p = GetShrunkSuffix(shrunk_p)) {
+        UpdateEntries(shrunk_p, char_pos, pattern_entries);
+    }
+}
+
+auto TemplateFinder::UpdateEntries(const std::shared_ptr<Node> &current_p, size_t char_pos,
+                                   std::vector<size_t> &pattern_entries) -> void
+{
+    auto update_entries = [current_p, char_pos, &pattern_entries](size_t bias) {
+        auto pattern_pos = static_cast<int64_t>(char_pos - bias - current_p->word_size_ + 1);
+        if (pattern_pos >= 0 && pattern_pos < static_cast<int64_t>(pattern_entries.size())) {
+            ++pattern_entries[static_cast<size_t>(pattern_pos)];
+        }
+    };
+    std::for_each(current_p->word_bias_.begin(), current_p->word_bias_.end(), update_entries);
+}
+
+int main() {
+    std::string text_template;
+    std::string text;
+    std::cin >> text_template >> text;
+
+    TemplateFinder finder(text_template, '?');
+
+    auto out_iter = std::ostream_iterator<size_t>(std::cout, " ");
+    finder.FindEntries(text, out_iter);
+
+    std::cout << std::endl;
+    return 0;
+}
--- module-4-task-c.cpp
+++ module-4-task-c.cpp
@@ -1,8 +1,8 @@
 #include <algorithm>
+#include <cassert>
 #include <iostream>
 #include <unordered_set>
 #include <vector>
-
 
 class GameSolution {
 private:
@@ -10,39 +10,35 @@ private:
     std::vector<size_t> positions_;
     std::vector<size_t> grandyFunction_;
 public:
-    explicit GameSolution(size_t n) : n_(n) { grandyFunction_.resize(n_ + 1); }
+    explicit GameSolution(size_t n);
     void Solve();
+    std::vector<size_t> GetTrivialPositions(size_t n) const;
     std::vector<size_t> GetPositions();
 };
 
 void GameSolution::Solve() {
-    grandyFunction_[0] = 0;
-    grandyFunction_[1] = 1;
-    grandyFunction_[2] = 1;
-
     for(size_t i = 3; i < n_ + 1; ++i) {
         std::unordered_set<size_t> mex;
         for(size_t j = 3; j < i - 1; ++j) {
-            mex.insert(grandyFunction_[j - 1] ^ grandyFunction_[i - j]);
+            size_t result = grandyFunction_[j - 1] ^ grandyFunction_[i - j];
 
-            if(i == n_ && (!(grandyFunction_[j - 1] ^ grandyFunction_[i - j]))) {
+            mex.insert(result);
+            if(i == n_ && !result) {
                 positions_.push_back(j);
             }
         }
 
         mex.insert(grandyFunction_[i - 1]);
         if(i == n_ && (!grandyFunction_[i - 1])) {
-            positions_.push_back(1);
-            positions_.push_back(n_);
+            positions_.insert(positions_.end(), {1, n_});
         }
 
         if(i == 3) {
             mex.insert(grandyFunction_[0]);
         } else {
             mex.insert(grandyFunction_[i - 2]);
-            if(i == n_ && (!grandyFunction_[i-2])) {
-                positions_.push_back(2);
-                positions_.push_back(i-1);
+            if(i == n_ && (!grandyFunction_[i - 2])) {
+                positions_.insert(positions_.end(), {2, i-1});
             }
         }
 
@@ -57,30 +53,29 @@ void GameSolution::Solve() {
 }
 
 std::vector<size_t> GameSolution::GetPositions() {
+    if(n_ < 4) {
+        return GetTrivialPositions(n_);
+    }
     return positions_;
+}
+
+std::vector<size_t> GameSolution::GetTrivialPositions(size_t n) const {
+    if(n == 1) return {1};
+    if(n == 2) return {1, 2};
+    if(n == 3) return {2};
+    assert(false);
+}
+
+GameSolution::GameSolution(size_t n) : n_(n) {
+    grandyFunction_.resize(n_ + 1);
+    grandyFunction_[0] = 0;
+    grandyFunction_[1] = 1;
+    grandyFunction_[2] = 1;
 }
 
 int main() {
     size_t n = 0;
     std::cin >> n;
-
-    if(n == 1) {
-        std::cout << "Schtirlitz\n";
-        std::cout << "1\n";
-        return 0;
-    }
-
-    if(n == 2) {
-        std::cout << "Schtirlitz\n";
-        std::cout << "1\n2\n";
-        return 0;
-    }
-
-    if(n == 3) {
-        std::cout << "Schtirlitz\n";
-        std::cout << 2;
-        return 0;
-    }
 
     GameSolution solution(n);
     solution.Solve();
